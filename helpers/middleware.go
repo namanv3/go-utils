67daf462d61873c8a6a3847471e4fd7f60afc798
Naman Verma <naman13verma@gmail.com>
2025-01-09 17:52:13 +0530

    Cors handler middleware builder for custom allowed headers

--- helpers/middleware.go
+++ helpers/middleware.go
@@ -24,17 +24,28 @@ func HandlerNameMiddleware(next http.Handler) http.Handler {
 }
 
 func CorsHandlerMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	return CorsHandlerMiddlewareForHeaders(nil)(next)
 
-		// Preflight requests
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
+}
 
-		next.ServeHTTP(w, r)
-	})
+func CorsHandlerMiddlewareForHeaders(allowedHeaders *string) func(next http.Handler) http.Handler {
+	headers := "Content-Type, Authorization"
+	if allowedHeaders != nil {
+		headers = *allowedHeaders
+	}
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", headers)
+
+			// Preflight requests
+			if r.Method == "OPTIONS" {
+				w.WriteHeader(http.StatusOK)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		})
+	}
 }
